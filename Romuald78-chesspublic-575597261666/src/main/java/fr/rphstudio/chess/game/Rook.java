bcbde70895c23abb2cef41316a83d8e3f5af11a8
Vlad <h.debelsunce@hotmail.fr>
2019-03-13 17:16:02 +0100

    Rook

--- Romuald78-chesspublic-575597261666/src/main/java/fr/rphstudio/chess/game/Rook.java
+++ Romuald78-chesspublic-575597261666/src/main/java/fr/rphstudio/chess/game/Rook.java
@@ -1,5 +1,61 @@
 package fr.rphstudio.chess.game;
 
-public class Rook {
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.rphstudio.chess.interf.IChess;
+import fr.rphstudio.chess.interf.IChess.ChessPosition;
+
+public class Rook implements IMove{
+	
+	/**
+	 *
+	 *
+	 *
+	 */
+	
+	public List<IChess.ChessPosition> getPossibleMoves(IChess.ChessPosition p, Plateau plat){
+		
+		ArrayList<ChessPosition> possibleMoves = new ArrayList<ChessPosition>();
+		
+		ChessPosition tmpPos;
+		
+		//horizontale
+		for(int i=1;i<IChess.BOARD_HEIGHT;i++)
+		{
+			tmpPos = new ChessPosition(p.x-i, p.y);
+			if(tmpPos.x >= 0 && tmpPos.x < IChess.BOARD_WIDTH && tmpPos.y >= 0 && tmpPos.y < IChess.BOARD_HEIGHT)
+			{
+				possibleMoves.add(tmpPos);
+			}
+		}
+		for(int i=1;i<IChess.BOARD_HEIGHT;i++)
+		{
+			tmpPos = new ChessPosition(p.x+i, p.y);
+			if(tmpPos.x >= 0 && tmpPos.x < IChess.BOARD_WIDTH && tmpPos.y >= 0 && tmpPos.y < IChess.BOARD_HEIGHT)
+			{
+				possibleMoves.add(tmpPos);
+			}
+		}
+		//Verticale
+		for(int i=1;i<IChess.BOARD_HEIGHT;i--)
+		{
+			tmpPos = new ChessPosition(p.x, p.y-i);
+			if(tmpPos.x >= 0 && tmpPos.x < IChess.BOARD_WIDTH && tmpPos.y >= 0 && tmpPos.y < IChess.BOARD_HEIGHT)
+			{
+				possibleMoves.add(tmpPos);
+			}
+		}
+		for(int i=1;i<IChess.BOARD_HEIGHT;i--)
+		{
+			tmpPos = new ChessPosition(p.x, p.y+i);
+			if(tmpPos.x >= 0 && tmpPos.x < IChess.BOARD_WIDTH && tmpPos.y >= 0 && tmpPos.y < IChess.BOARD_HEIGHT)
+			{
+				possibleMoves.add(tmpPos);
+			}
+		}
+
+	return possibleMoves;
+	}
 
 }
